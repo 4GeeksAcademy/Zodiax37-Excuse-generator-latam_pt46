d474f7dd2a2da910b4bf594cf64ebb92b41febb9
Zodiax37 <98854282+Zodiax37@users.noreply.github.com>
2025-02-13 03:08:17 +0000

    Realizar la funcion OnLoad

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
+/* import "bootstrap";
 import "./style.css";
+*/
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoItem = Math.floor(Math.random()*who.length)
+  
+  let actionItem = Math.floor(Math.random()*action.length)
+  
+  let whatItem = Math.floor(Math.random()*what.length)
+  
+  let whenItem = Math.floor(Math.random()*when.length)
+
+  let newExcuse = `${who[whoItem]} ${action[actionItem]} ${what[whatItem]} ${when[whenItem]}!`
+
+
+  const p = document.getElementById("excuse")
+  p.innerText = newExcuse
+
+  console.log(newExcuse);
 };
